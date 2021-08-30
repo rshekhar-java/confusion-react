f7eeee05a387152775c681fe6454bb526a9547fd
Ravi Shekhar <rsshekhar27@outlook.com>
2021-08-30 01:58:20 -0500

    Header and Footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,16 +18,20 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
     const Menu = (props) => {
         const menu = props.dishes.map((dish) => {
             return (
-              <div  className="col-12 col-md-5 m-1">
-                  <RenderMenuItem dish={dish} onClick={props.onClick} />
-              </div>
+                <div  className="col-12 col-md-5 m-1">
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
+             
             );
         });
 
         return (
 
-            <div className="row">
-                {menu}   
+            
+            <div className="container">  
+                <div className="row">
+                    {menu}   
+                </div>
             </div>
         );
     }
