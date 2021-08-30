1150839d4541124e9fcd2f69daf22b527fe33851
Ravi Shekhar <rsshekhar27@outlook.com>
2021-08-30 15:46:27 -0500

    Single Page Applications Part 1

---
+++ src/components/ContactComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Contact(props) {
+    return(
+        <div className="container">
+            <div className="row row-content">
+                <div className="col-12">
+                <h3>Location Information</h3>
+                </div>
+                <div className="col-12 col-sm-4 offset-sm-1">
+                        <h5>Our Address</h5>
+                        <address>
+                        121, Clear Water Bay Road<br />
+                        Clear Water Bay, Kowloon<br />
+                        HONG KONG<br />
+                        <i className="fa fa-phone"></i>: +1 314 445 7890<br />
+                        <i className="fa fa-fax"></i>: +1 314 478 6789<br />
+                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                        </address>
+                </div>
+                <div className="col-12 col-sm-6 offset-sm-1">
+                    <h5>Map of our Location</h5>
+                </div>
+                <div className="col-12 col-sm-11 offset-sm-1">
+                    <div className="btn-group" role="group">
+                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-success"><i className="fa fa-whatsapp"></i> Whatsapp</a>
+                        <a role="button" className="btn btn-warning" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Contact;
